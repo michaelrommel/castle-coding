fa0188ecadfa2a59c87acf198011ff28bb651d0e
Michael Rommel <rommel@layer-7.net>
2024-12-02 12:01:11 +0100

    fix: remove pnpm again, wrong installation path

--- home/.path.d/35_pnpm.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-export PNPM_HOME="${HOME}/.local/share/pnpm"
-if [[ -d "${PNPM_HOME}" && ! ":${PATH}:" == *":${PNPM_HOME}:"* ]]; then
-	# path has not yet been added
-	export PATH="${PNPM_HOME}:${PATH}"
-fi

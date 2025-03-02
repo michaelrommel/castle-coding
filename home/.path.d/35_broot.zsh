7f1587f3cc2ecf938385c5c21fbce0e7d268b8e9
Michael Rommel <rommel@layer-7.net>
2025-03-02 22:57:56 +0100

    feat: add broot

---
+++ home/.path.d/35_broot.zsh
@@ -0,0 +1,26 @@
+#! /usr/bin/env zsh
+
+RET=$(broot --version 2>/dev/null)
+if [[ $? -ne 0 ]]; then
+	exit 0
+fi
+
+# This function starts broot and executes the command
+# it produces, if any.
+# It's needed because some shell commands, like `cd`,
+# have no useful effect if executed in a subshell.
+function br {
+	local cmd cmd_file code
+	cmd_file=$(mktemp)
+	if broot --outcmd "$cmd_file" "$@"; then
+		cmd=$(<"$cmd_file")
+		command rm -f "$cmd_file"
+		eval "$cmd"
+	else
+		code=$?
+		command rm -f "$cmd_file"
+		return "$code"
+	fi
+}
+
+broot --set-install-state installed
